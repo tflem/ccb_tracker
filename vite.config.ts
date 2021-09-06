2e74da796ec11e715a45b8e24d15fb1b1fc0373a
Tim Fleming <tfleming26@hotmail.com>
2021-09-06 11:30:52 -0600

    Create welcome page and install typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import WindiCSS from 'vite-plugin-windicss'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    WindiCSS(),
+    WindiCSS({
+      root: __dirname,
+      scan: {
+        fileExtensions: [
+          "erb",
+          "haml",
+          "html",
+          "vue",
+          "js",
+          "ts",
+          "jsx",
+          "tsx",
+        ],
+        dirs: ["app/views", "app/javascript"],
+      },
+    }),
   ],
-})
+});
