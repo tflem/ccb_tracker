389004fb1cc22cdf207272583a3219ca6fb83d16
Tim Fleming <tfleming26@hotmail.com>
2021-10-13 11:33:28 -0600

    Remove boilerplate from request spec

--- spec/requests/balances_spec.rb
+++ spec/requests/balances_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Balances", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /index" do    
   end
 end
