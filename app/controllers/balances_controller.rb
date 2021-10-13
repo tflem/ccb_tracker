94b9ebd6eb7c03c3dc65f424a87b1c07930354f4
Tim Fleming <tfleming26@hotmail.com>
2021-10-13 10:05:56 -0600

    Add a create action to balances controller, update new action in balances controller, and add a form to new template

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -3,5 +3,17 @@ class BalancesController < ApplicationController
   end
 
   def new
+    @balance = Balance.new
+  end
+
+  def create
+    @balance = Balance.new(balance_params)
+
+    if @balance.save
+      flash[:notice] = "Your credit card balance has been created."
+      redirect_to @balance
+    else
+      #notta notta yet
+    end
   end
 end
