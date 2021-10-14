4a13a796812af3e15774a800a5ab2bdb2f25d2c2
Tim Fleming <tfleming26@hotmail.com>
2021-10-14 11:42:43 -0600

    Create title functionality for balance show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title_balance_show(*items)
+    unless items.empty?
+      content_for :title_balance_show do
+        (items << "CCB Tracker").join(" - ")
+      end
+    end
+  end
 end
--- spec/features/creating_balances_spec.rb
+++ spec/features/creating_balances_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe "Users can create new balance data" do
     click_button "Create Balance"
 
     expect(page).to have_content "Your credit card balance has been created."
+
+    balance = Balance.find_by!(name: "Costco Credit Card")
+    expect(page.current_url).to eq balance_url(balance)
+
+    title = "Costco Credit Card - Balances - CCB Tracker"
+    expect(page).to have_title title
   end
 end
