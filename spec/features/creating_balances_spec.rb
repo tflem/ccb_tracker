2c942445355048f0c5b570ee159e90be155f52e1
Tim Fleming <tfleming26@hotmail.com>
2021-10-20 15:13:34 -0600

    Add spec for testing validations as well as error messages in form

--- spec/features/creating_balances_spec.rb
+++ spec/features/creating_balances_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Users can create new balance data" do
-  scenario "with valid attributes" do
+  before do
     visit "/"
 
     click_link "Learn More"
+  end
 
+  scenario "with valid attributes" do
     fill_in "Name", with: "Costco Credit Card"
     fill_in "Balance", with: 123.99
     click_button "Create Balance"
@@ -17,5 +19,13 @@ RSpec.describe "Users can create new balance data" do
 
     title = "Costco Credit Card - Balances - CCB Tracker"
     expect(page).to have_title title
+  end
+
+  scenario "when providing invalid balance data" do
+    click_button "Create Balance"
+
+    expect(page).to have_content "Your credit card balance has not been created."
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Balance can't be blank"
   end
 end
