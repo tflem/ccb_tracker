e63fe294f18a36071d1c10a813461301a6e8bc98
Tim Fleming <tfleming26@hotmail.com>
2021-12-05 09:20:33 -0600

    Create spec for viewing notes and notes factory

---
+++ spec/factories/notes.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :note do
+    name { "Example note" }
+    description { "An example note within a balance" }
+  end    
+end
---
+++ spec/features/viewing_notes_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Our users can view notes" do
+  before do
+    costco_balance = FactoryBot.create(:balance, name: "Costco Credit Card",
+                                          amount: 67.45)
+    FactoryBot.create(:note, balance: costco_balance, 
+                             name: "Gas Card",
+                             description: "This is a gas card provided by Costco.")
+
+    amex_balance = FactoryBot.create(:balance, name: "American Express Card",
+                                               amount: 1238.54)
+    FactoryBot.create(:note, balance: amex_balance,
+                             name: "AMEX Travel Card",
+                             description: "American Express card for travel expenses.")
+
+    visit "/"   
+  end
+  
+  scenario "for any particular balance" do
+    click_link "View Balances"
+    click_link "Costco Credit Card"
+
+    expect(page).to have_content "Gas Card"
+    expect(page).to_not have_content "Amex Travel Card"
+
+    click_link "Gas Card"   
+    
+    expect(page).to have_content "Gas Card"
+    expect(page).to have_content "This is a gas card provided by Costco."    
+  end
+end
