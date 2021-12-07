4e1b984caefc5e49ec33bb1260d81678cc8aedcb
Tim Fleming <tfleming26@hotmail.com>
2021-12-07 15:27:56 -0600

    Notes are now deleted with balances

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,5 +1,5 @@
 class Balance < ApplicationRecord
-  has_many :notes
+  has_many :notes, dependent: :delete_all
   validates :name, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0.01 }
 end
