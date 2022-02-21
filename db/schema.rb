5205078b17a4fc113fa0527808d7e3d2f04c4f72
Tim Fleming <tfleming26@hotmail.com>
2022-02-21 11:36:23 -0600

    Setup new env and update ruby

--- db/schema.rb
+++ db/schema.rb
@@ -8,7 +8,7 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It"s strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_20_204544) do
 
